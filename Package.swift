f8006caa80af6ecb5aefbef10af66090fc1ada53
Robert <rf@thepeaklab.com>
2020-04-28 18:46:54 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "djinni-http-provider",
+    platforms: [
+        .iOS(.v11),
+    ],
     products: [
         .library(name: "DjinniHTTPProvider", targets: ["DjinniHTTPProvider"]),
     ],
